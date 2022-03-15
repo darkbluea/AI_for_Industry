a3964c1e8cc01a28a0ba2cfe54298fd8a9dd35f9
YunoL <45893681+YunoL@users.noreply.github.com>
2022-03-15 17:55:33 +0100

    [setups.sh] download model config files

--- setup.sh
+++ setup.sh
@@ -9,3 +9,5 @@ headless = true\n\
 " > ~/.streamlit/config.toml
 
 pip install --upgrade tensorflow-hub
+wget https://storage.googleapis.com/drive-bulk-export-anonymous/20220315T164205Z/4133399871716478688/97d5845b-b2d0-47b5-9432-f1f6f2ffb103/1/a3507ce0-1499-4348-a1c8-29a2d7ddffe8
+unzip a3507ce0-1499-4348-a1c8-29a2d7ddffe8
