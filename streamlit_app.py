4026fef1491d5ed011132d492aade9b0cda42811
darkbluea <50586316+darkbluea@users.noreply.github.com>
2022-03-15 16:58:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,21 +36,8 @@ def load_model(modelfile):
 #    resnet_model2.load_weights(modelfile)
 #    return resnet_model2
 
-def test_picture(model, path):
-    df = pd.DataFrame(path, columns = ['Path'])
-    df['Label'] = [re.findall('[0-9]{4}_(.+?).png', single_path)[0] for single_path in path]
-    test_generator = ImageDataGenerator(rescale = 1./255,)
-    test = test_generator.flow_from_dataframe(dataframe = df,
-                                    class_mode  = 'binary',
-                                    x_col       = 'Path',
-                                    y_col       = 'Label',
-                                    shuffle     = False,
-                                    batch_size  = 32,
-                                    target_size = (224, 224))
-    temp = [(float(prob), True if prob > 0.33 else False) for prob in model.predict(test)]
-    return [(path[i], temp[i][0], temp[i][1]) for i in range(len(path))]
 
-st.set_page_config(page_title="Neural Network prediction on patient lungs", page_icon="https://www.freeiconspng.com/thumbs/heart-png/heart-png-15.png", layout='centered', initial_sidebar_state="collapsed")
+st.set_page_config(page_title="AI for Industry", page_icon="", layout='centered', initial_sidebar_state="collapsed")
 
 def main(model):
     html_temp = """
@@ -59,37 +46,6 @@ def main(model):
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
-    paths_good = [
-	        "png/MCUCXR_0001_0.png",
-	        "png/MCUCXR_0002_0.png",
-	        "png/MCUCXR_0003_0.png",
-	        "png/MCUCXR_0004_0.png",
-	        "png/MCUCXR_0005_0.png",
-    ]
-    paths_bad = [
-	        "png/MCUCXR_0104_1.png",
-	        "png/MCUCXR_0108_1.png",
-	        "png/MCUCXR_0113_1.png",
-	        "png/MCUCXR_0117_1.png",
-	        "png/MCUCXR_0126_1.png",
-    ]
-    
-    results = test_picture(model, paths_good + paths_bad)
-    i = 0
-    col_list_1 = st.columns(5)
-    col_list_2 = st.columns(5)
-    def prompt_result(result, st_place):
-        image = Image.open(result[0])
-        caption = "prob = " + str(result[1]) + "\nresult = " + str(result[2])
-        st_place.image(image, caption=caption)
-    for result in results:
-        st_place = st
-        if i < 10:
-            st_place = col_list_2[i - 5]
-        if i < 5:
-            st_place = col_list_1[i]
-        prompt_result(result, st_place)
-        i += 1
 
 hide_menu_style = """
         <style>
